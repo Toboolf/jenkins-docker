2325fb1a6d26b234dccccc7732b46a07660197b0
José Cruz <jf.cruz001@ebc.edu.mx>
2021-11-04 02:28:25 -0600

    coding master updating init shell to create a volume and start both containers

--- init-jenkins.sh
+++ init-jenkins.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+mkdir $HOME/jenkins
+sudo chgrp docker $HOME/jenkins
+sudo chown -R floobot:docker $HOME/jenkins
+
 docker network create jenkins
 docker run \
   --name jenkins-docker \
@@ -9,8 +13,24 @@ docker run \
   --network jenkins \
   --network-alias docker \
   --env DOCKER_TLS_CERTDIR=/certs \
-  --volume jenkins-docker-certs:/certs/client \
-  --volume jenkins-home:/var/jenkins_home \
+  --volume $HOME/jenkins/jenkins-docker-certs:/certs/client \
+  --volume $HOME/jenkins/jenkins-home:/var/jenkins_home \
   --publish 2376:2376 \
   docker:dind \
   --storage-driver overlay2
+
+docker build -t custom-jenkins:0.1 .
+
+docker run \
+  --name custom-jenkins \
+  --rm \
+  --detach \
+  --network jenkins \
+  --env DOCKER_HOST=tcp://docker:2376 \
+  --env DOCKER_CERT_PATH=/certs/client \
+  --env DOCKER_TLS_VERIFY=1 \
+  --publish 8080:8080 \
+  --publish 50000:50000 \
+  --volume $HOME/jenkins/jenkins-home:/var/jenkins_home \
+  --volume $HOME/jenkins/jenkins-docker-certs:/certs/client:ro \
+  custom-jenkins:0.1
